a5523eb4e8cec780ab206a82ef8d01562de10ed1
Luiz Ernandes <84932111+LuizSoares1@users.noreply.github.com>
2023-01-25 15:14:16 -0300

    A little changes.

--- app/validacao.js
+++ app/validacao.js
@@ -17,8 +17,7 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
-
-            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente!</button>
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
@@ -29,7 +28,6 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
- 
 }
 
 function chuteForInvalido(numero) {
